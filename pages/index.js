d93e2a1b1f5f76c5e1de891c10f5269d8324ae02
chiemgiabaost <101836870+chiemgiabaost@users.noreply.github.com>
2024-11-14 20:40:52 -0500

    Rebased to the new github

---
+++ pages/index.js
@@ -0,0 +1,20 @@
+import {useSession,signIn, signOut} from "next-auth/react";
+
+export default function Home() {
+  const {data: session} = useSession();
+
+  if (!session) {
+    return (
+      <div className="bg-gradient-to-r from-black to-blue-900 w-screen h-screen flex justify-center items-center">
+        <div className="text-blue-900 flex justify-between">
+          <a href="/api/auth/signin" className="text-white hover:underline">Sign in</a>
+        </div>
+      </div>
+    )
+  }
+  return (
+    <div>
+      Logged in as {session.user.email} <button onClick={() => signOut()}>Sign out</button>
+    </div>
+  )
+}
