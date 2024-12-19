d2721d9c644138dbb5935cd89e2f4fb6633971bb
chiemgiabaost <bao2003@my.yorku.ca>
2024-12-19 15:34:13 -0500

    fix bug in images uploading

--- pages/api/upload.js
+++ pages/api/upload.js
@@ -1,41 +1,52 @@
-import { PutObjectCommand, S3, S3Client } from '@aws-sdk/client-s3'; 
-import fs from 'fs';
+import { PutObjectCommand, S3Client } from '@aws-sdk/client-s3';
 import multiparty from 'multiparty';
 import mime from 'mime-types';
+import stream from 'stream';
+import util from 'util';
+import fs from 'fs';
+
+const pipeline = util.promisify(stream.pipeline);
 
 export default async function handle(req, res) {
     const bucketName = "next-ecommerce-bao";
     const form = new multiparty.Form();
-    const {fields, files} =  await new Promise((resolve, reject) => {
-        form.parse(req, async (err, fields, files) => {
-            if (err) throw err;
-            resolve({fields, files});           
-        
-    })
+    
+    const {fields, files} = await new Promise((resolve, reject) => {
+        form.parse(req, (err, fields, files) => {
+            if (err) reject(err);
+            resolve({fields, files});
+        });
     });
-    console.log("Length: ", files.file.length);
+
     const client = new S3Client({
         region: "ca-central-1",
         credentials: {
             accessKeyId: process.env.S3_ACCESS_KEY,
             secretAccessKey: process.env.S3_SECRET_ACCESS_KEY
         }
-    })
+    });
+
     const links = [];
-    for ( const file of files.file){
+    for (const file of files.file) {
         const ext = file.originalFilename.split('.').pop();
         const newFileName = Date.now() + '.' + ext;
-        console.log({ext, file});
+
+        // Create a readable stream from the uploaded file
+        const fileStream = fs.createReadStream(file.path);
+
+        // Upload the stream directly to S3
         await client.send(new PutObjectCommand({
             Bucket: bucketName,
             Key: newFileName,
-            Body: fs.readFileSync(file.path),
+            Body: fileStream,
             ACL: "public-read",
             ContentType: mime.lookup(file.path)
         }));
+
         const link = `https://${bucketName}.s3.amazonaws.com/${newFileName}`;
         links.push(link);
     }
+
     return res.json({links});
 }
 
@@ -43,4 +54,4 @@ export const config = {
     api: {
         bodyParser: false
     }
-}
+};
